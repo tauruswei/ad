0ccf2e8201aa83474bc31482a1adb4e94d7d87a6
francesLy <1096074393@qq.com>
2023-09-19 11:46:37 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -7,27 +7,20 @@ import { MetaMask } from "./utils/meta-mask";
 //require("../mock/index.js");
 import { Tab, Tabs, NavBar, Button, Col, Row, Image as VanImage, Tag, NumberKeyboard, Field, CellGroup, Dialog, Icon, Grid, GridItem, Popup, List, Cell, Empty, PullRefresh, TextEllipsis } from 'vant';
 const app = createApp(App);
+import VConsole from 'vconsole';
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV == "development") {
+    //手机端调试
+    const vConsole = new VConsole();
+    //app.use(vConsole)
+    console.log(11111)
+}
 app.use(Tab).use(Tabs).use(NavBar).use(Button).use(Col).use(Row)
     .use(VanImage).use(Tag).use(NumberKeyboard).use(Field).use(CellGroup)
     .use(Dialog).use(Icon).use(Grid).use(GridItem).use(Popup).use(List)
     .use(Empty).use(Cell).use(PullRefresh).use(TextEllipsis)
 app.config.globalProperties.metaMask = new MetaMask();
-app.use(store).use(router)
-
-import VConsole from 'vconsole';
-if (process.env.NODE_ENV == "test") {
-    //手机端调试
-
-    const vConsole = new VConsole({ // 配置选项 
-        defaultPlugins: ['system', 'network', 'element'],
-        // 默认开启的插件 
-        onReady() {
-            console.log('vConsole is ready!');
-        },
-    });
-    app.use(vConsole)
-}
-app.mount("#app");
+app.use(store).use(router).mount("#app");
 
 app.config.errorHandler = (err, vm, info) => {
     console.log(err, vm, info)
