c8b47f8ff184de864e4e04e9de4251de7194a690
魏冰涛 <13156050650@163.com>
2023-09-21 13:29:34 +0800

    生产环境也可以使用 vConsole 模式

--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@ import { Tab, Tabs, NavBar, Button, Col, Row, Image as VanImage, Tag, NumberKeyb
 const app = createApp(App);
 import VConsole from 'vconsole';
 console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV == "development") {
+// if (process.env.NODE_ENV == "development") {
     //手机端调试
     const vConsole = new VConsole();
     //app.use(vConsole)
-    console.log(11111)
-}
+    console.log(11111);
+// }
 app.use(Tab).use(Tabs).use(NavBar).use(Button).use(Col).use(Row)
     .use(VanImage).use(Tag).use(NumberKeyboard).use(Field).use(CellGroup)
     .use(Dialog).use(Icon).use(Grid).use(GridItem).use(Popup).use(List)
